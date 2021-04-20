97f495c4660daed39001209fa6b6b1032ccc4c1a
sophieehill <43281937+sophieehill@users.noreply.github.com>
2021-04-20 12:50:25 -0400

    .

--- code.R
+++ code.R
@@ -14,7 +14,6 @@ people <- people[order(people$id),]
 # calculate degree cenrality in order to scale nodes
 graph <- igraph::graph.data.frame(connections, directed = F)
 degree_value <- degree(graph, mode = "in")
-sort(degree_value)
 # scaling factor 
 people$icon.size <- degree_value[match(people$id, names(degree_value))] + 25
 big.icons <- c("UK government", "Conservative party")
@@ -72,20 +71,5 @@ connections$value <- as.integer(as.character(connections$value))
 save(people, file = "people.RData")
 save(connections, file = "connections.RData")
 
-visNetwork(people, connections) %>%
-  visEdges(scaling=list(min=8, max=50)) %>%
-  visNodes(scaling=list(min=50, max=500)) %>%
-  visOptions(highlightNearest = list(enabled = T, degree = 1, hover = T)) %>%
-  visInteraction(hover=TRUE, zoomView = TRUE,
-                 multiselect=TRUE,
-                 navigationButtons = FALSE,
-                 tooltipStyle = 'position: fixed;visibility:hidden;padding: 5px;
-                font-family: sans-serif;font-size:14px;font-color:#000000;background-color: #e3fafa;
-                -moz-border-radius: 3px;-webkit-border-radius: 3px;border-radius: 3px;
-                 border: 0px solid #808074;box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                 max-width:200px;overflow-wrap: normal') %>%
-  visPhysics(solver = "forceAtlas2Based", forceAtlas2Based = list(gravitationalConstant = -30)) %>%
-  addFontAwesome() %>%
-  visLayout(randomSeed = 02143)
 
 
